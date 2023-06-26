24e80291e43bfbf993af670fcd3f6388986130d1
sky46 <sky.ma.ottawa@gmail.com>
2023-06-26 14:24:37 -0400

    add missing addTemplateFormats for scss in 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
+  
+  eleventyConfig.addTemplateFormats("scss");
   eleventyConfig.addExtension("scss", {
     outputFileExtension: "css",
     compile: async function (inputContent) {
