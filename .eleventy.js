70da59b85c0957693cb3c14c310a3cb23764bb31
sky46 <skyler@skylerma.com>
2025-07-13 00:42:48 -0400

    Big scss refactor; adjust styles and page contents

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,10 @@
-const sass = require("sass");
+
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   
-  eleventyConfig.addTemplateFormats("scss");
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css",
-    compile: async function (inputContent) {
-      let result = sass.compileString(inputContent);
-      return async (data) => {
-        return result.css;
-      };
-    },
-  });
-  
+  eleventyConfig.addPassthroughCopy("assets/css");
   eleventyConfig.addPassthroughCopy("assets/img");
   eleventyConfig.addPassthroughCopy("assets/pdf");
   eleventyConfig.addPassthroughCopy("assets/js");
